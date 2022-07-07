016ec793a67e50a3850cc3e3b0ca36b2fbe2882c
Cornelius Ukena <guy@cornelius.dev>
2022-07-06 20:51:37 -0500

    🚧 minor additions

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const fs = require('fs');
+const path = require('path');
 const dotenv = require('dotenv');
 const { defineConfig } = require('vite');
 const tinify = require('./vite-plugin-tinify');
-const { default: viteRestart } = require('vite-plugin-restart');
+const { default: svgo } = require('vite-svg-loader');
+const { default: restart } = require('vite-plugin-restart');
 const { default: devManifest } = require('vite-plugin-dev-manifest');
 const { svelte } = require('@sveltejs/vite-plugin-svelte');
 
 const __home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 const pem = `${__home}/.config/valet/CA/LaravelValetCASelfSigned.pem`;
+const certDir = `${__home}/.config/valet/Certificates`;
+const HOST = process.env.APP_HOST;
+const URL = process.env.APP_URL;
 
 try {
-    (env => Object.keys(env).forEach(key => {
-        process.env[key] = env[key].replace(/\$\{(.+)\}/gi, (original, a) => env[a]);
-    }))(dotenv.config().parsed);
+    const env = dotenv.config().parsed;
+
+    Object.keys(env).forEach(key => {
+        process.env[key] = env[key].replace(
+            /\$\{(.+)\}/gi, (original, a) => env[a]
+        );
+    });
 } catch (error) {
     console.warn('No .env file');
 }
@@ -20,33 +29,29 @@ try {
 module.exports = (args = {}, handler) => {
     return handler(defineConfig({
         plugins: [
-            viteRestart({
-                restart: args.watch
-                    ? args.watch
-                    : []
-            }),
+            restart({ restart: args.watch ? args.watch : [] }),
+            svgo({ defaultImport: 'url' }),
             devManifest(),
             tinify(),
             svelte()
         ],
         base: args.base,
-        publicDir: args.static
-            ? args.static
-            : false,
+        publicDir: args.static ? args.static : false,
         build: {
             manifest: true,
             outDir: args.outDir
         },
         server: {
-            host: process.env.APP_HOST ? process.env.APP_HOST : 'localhost',
-            https: args.https ? args.https : (process.env.APP_URL.includes('https:') && fs.existsSync(pem) ? {
-                key: fs.readFileSync(`${__home}/.config/valet/Certificates/${process.env.APP_HOST}.key`),
-                cert: fs.readFileSync(`${__home}/.config/valet/Certificates/${process.env.APP_HOST}.crt`),
+            host: HOST ? HOST : 'localhost',
+            https: args.https ? args.https : (URL.includes('https:') && fs.existsSync(pem) ? {
+                key: fs.readFileSync(`${certDir}/${HOST}.key`),
+                cert: fs.readFileSync(`${certDir}/${HOST}.crt`),
                 ca: fs.readFileSync(pem)
             } : false)
         },
         resolve: {
-            extensions: [ '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.svelte' ]
+            extensions: [ '.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.svelte' ],
+            alias: { '@': path.resolve(process.cwd(), 'src') }
         }
     }));
 };
