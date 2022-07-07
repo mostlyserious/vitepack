119ad4830688fa91cc9794711d883b1d43abd8d3
Cornelius Ukena <guy@cornelius.dev>
2022-07-06 21:01:51 -0500

    🐛 fix last update

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const { svelte } = require('@sveltejs/vite-plugin-svelte');
 const __home = process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE;
 const pem = `${__home}/.config/valet/CA/LaravelValetCASelfSigned.pem`;
 const certDir = `${__home}/.config/valet/Certificates`;
-const HOST = process.env.APP_HOST;
-const URL = process.env.APP_URL;
 
 try {
     const env = dotenv.config().parsed;
@@ -27,22 +25,25 @@ try {
 }
 
 module.exports = (args = {}, handler) => {
+    const HOST = process.env.APP_HOST;
+    const URL = process.env.APP_URL;
+
     return handler(defineConfig({
+        base: args.base,
+        publicDir: (args.static ? args.static : false),
+        build: {
+            manifest: true,
+            outDir: args.outDir
+        },
         plugins: [
-            restart({ restart: args.watch ? args.watch : [] }),
+            restart({ restart: (args.watch ? args.watch : []) }),
             svgo({ defaultImport: 'url' }),
             devManifest(),
             tinify(),
             svelte()
         ],
-        base: args.base,
-        publicDir: args.static ? args.static : false,
-        build: {
-            manifest: true,
-            outDir: args.outDir
-        },
         server: {
-            host: HOST ? HOST : 'localhost',
+            host: (HOST ? HOST : 'localhost'),
             https: args.https ? args.https : (URL.includes('https:') && fs.existsSync(pem) ? {
                 key: fs.readFileSync(`${certDir}/${HOST}.key`),
                 cert: fs.readFileSync(`${certDir}/${HOST}.crt`),
